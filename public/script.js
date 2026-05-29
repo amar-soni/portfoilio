b8edd39aba45e6d635f649e55c6cf65f5759ff76
Amar Soni <d.amarsoni10@gmail.com>
2026-05-30 01:26:27 +0530

    url fetch

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
-  const response = await fetch("http://localhost:5000/contact", {
+  const response = await fetch("https://portfoilio-oebl.onrender.com/contact", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
